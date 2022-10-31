b4cf5bfe148cda49df8a82cf8c640f1a7af8d504
Yoochanhong <chanhongy6@naver.com>
2022-10-31 17:36:47 +0900

    두번째 페이지

---
+++ 라우트 관리/get_off/lib/next_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget {
+  const NextPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('두번째 페이지'),
+      ),
+      body: Center(
+        child: Text(
+          'Get off 성공',
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
